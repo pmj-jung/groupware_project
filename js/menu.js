a8d021e180d0bd1cba2082172f5cf1b8f64c37fe
minjung <alswjddl4023@hanmail.net>
2020-09-15 11:36:29 +0900

    add js to logout N changePage

--- js/menu.js
+++ js/menu.js
@@ -10,5 +10,14 @@ $(function(){
         }
     });
 
+    $(".userInfo_detail").click(function(){
+        if($(this).find(".changeStatus").is(":visible")){
+            $(this).find(".changeStatus").slideUp(200);
+        }
+        else{
+            $(this).find(".changeStatus").slideDown(200);
+        }
+    });
+
     
 });
