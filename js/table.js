751003f651a4484732b44a4fc8bd8cdc2c97abcc
minjung <alswjddl4023@hanmail.net>
2020-09-18 17:01:54 +0900

    tr:nth-child(2n+1)를 css지우고 jquery로 동작

---
+++ js/table.js
@@ -0,0 +1,15 @@
+// $(function(){
+//     $("tr:not(:first-child)").mouseover(function(){
+//         $(this).addClass("hover");
+//     });
+//     $("tr").mouseout(function(){
+//         $(this).removeClass("hover");
+//     });
+// });
+
+$(function() {
+    $("tr:nth-child(2n+1)").addClass("tr-even");
+});
+
+
+    
