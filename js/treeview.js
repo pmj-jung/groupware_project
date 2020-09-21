4e263c8e0de32c5ad327a705ddcd582dd70e2b09
minjung <alswjddl4023@hanmail.net>
2020-09-21 16:39:44 +0900

    조직도관리 재작업, select-option에 아이디필요

--- js/treeview.js
+++ js/treeview.js
@@ -6,22 +6,10 @@ $(function() {
         persist: "location"
     });
 
-    $("#tree-dept").treeview({
+    $("#tree-org").treeview({
         collapsed: false,
         animated:0,
-        control: "#sidetreecontrol-dept",
-        persist: "location"
-    });
-    $("#tree-rank").treeview({
-        collapsed: false,
-        animated:0,
-        control: "#sidetreecontrol-rank",
-        persist: "location"
-    });
-    $("#tree-recruit").treeview({
-        collapsed: false,
-        animated:0,
-        control: "#sidetreecontrol-recruit",
+        control: "#sidetreecontrol-org",
         persist: "location"
     });
 });
