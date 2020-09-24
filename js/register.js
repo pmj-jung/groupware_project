f4eb645d93e9fe02257b82d5fb6029b11773f0a2
minjung <alswjddl4023@hanmail.net>
2020-09-24 17:13:48 +0900

    change variable name

--- js/register.js
+++ js/register.js
@@ -6,84 +6,56 @@ $(function(){
         var getName= RegExp(/^[가-힣]+$/);
 
         //아이디 유효성검사
-        if(!getCheck.test($("#user-id").val())){
+        if(!getCheck.test($("#userid").val())){
             alert("아이디는 영어 소문자와 숫자를 섞어 4~12자리로 입력해주세요.");
-            $("#user-id").val("");
-            $("#user-id").focus();
+            $("#userid").val("");
+            $("#userid").focus();
             return false;
         }
 
         //아이디 비밀번호 같음 확인
-        if($("#user-id").val() == $("#user-pwd").val()){
+        if($("#userid").val() == $("#pwd").val()){
             alert("아이디와 비밀번호가 같습니다");
-            $("#user-pwd").val("");
-            $("#user-pwd").focus();
-            return false;
-        }
-
-        //비밀번호 공백 확인
-        if($("#user-pwd").val() == ""){
-            alert("비밀번호를 입력해주세요");
-            $("#user-pwd").focus();
+            $("#pwd").val("");
+            $("#pwd").focus();
             return false;
         }
 
         //비밀번호 유효성검사
-        if(!getPwdCheck.test($("#user-pwd").val())){
+        if(!getPwdCheck.test($("#pwd").val())){
             alert("비밀번호는 8자 이상, 16자 이하여야 하며, 소문자와 숫자가 하나이상 포함되어야 합니다.");
-            $("#user-pwd").val("");
-            $("#user-pwd").focus();
-            return false;
-        }
-
-        //비밀번호 확인란 공백 확인
-        if($("#pwd-check").val() == ""){
-            alert("패스워드 확인란을 입력해주세요"); 
-            $("#pwd-check").focus();
+            $("#pwd").val("");
+            $("#pwd").focus();
             return false;
         }
         
         //비밀번호 서로확인
-        if($("#user-pw").val() != $("#pwd-check").val()){
+        if($("#pwd").val() != $("#pwd-check").val()){
             alert("비밀번호가 상이합니다");
-            $("#user-pw").val("");
+            $("#pwd").val("");
             $("#pwd-check").val("");
-            $("#user-pw").focus();
-            return false;
-        }
-
-        //이름 공백 검사
-        if($("#user-name").val() == ""){
-            alert("이름을 입력해주세요");
-            $("#user-name").focus();
+            $("#pwd").focus();
             return false;
         }
 
         //이름 유효성 검사
-        if(!getName.test($("#user-name").val())){
+        if(!getName.test($("#name").val())){
             alert("이름형식에 맞게 입력해주세요");
-            $("#user-name").val("");
-            $("#user-name").focus();
-            return false;
-        }
-
-        //주민번호 유효성 검사
-        if(($("#user-id-num").val() == "") || ($("#user-id-num_back").val() == "")){
-            alert("주민등록번호를 입력해주세요");
-            $("#user-id-num").focus(); 
+            $("#name").val("");
+            $("#name").focus();
             return false;
         }
 
         //주민등록번호 -> 생년월일 전달
-        var jumin = $("#user-id-num").val() + $("#user-id-num-back").val();
+        var jumin = $("#jumin").val() + $("#jumin-back").val();
         var fmt = RegExp(/^\d{6}[1234]\d{6}$/);
         var buf = new Array(13);
 
         if(!fmt.test(jumin)){
             alert("주민등록번호 형식에 맞게 입력해주세요.");
-            $("#user-id-num").focus();
-            $("#user-id-num").val("");
-            $("#user-id-num-back").val("");
+            $("#jumin").focus();
+            $("#jumin").val("");
+            $("#jumin-back").val("");
             return false;
         }
 
@@ -99,14 +71,14 @@ $(function(){
 
         if ((11 - (sum % 11)) % 10 != buf[12]) {
             alert("잘못된 주민등록번호 입니다.");
-            $("#user-id-num").focus();
+            $("#jumin").focus();
             return false;
         }
 
         var birthYear = (jumins.charAt(6) <= "2") ? "19" : "20";
-        birthYear += $("#user-id-num").val().substr(0, 2);
-        var birthMonth = $("#user-id-num").val().substr(2, 2);
-        var birthDate = $("#user-id-num").val().substr(4, 2);
+        birthYear += $("#jumin").val().substr(0, 2);
+        var birthMonth = $("#jumin").val().substr(2, 2);
+        var birthDate = $("#jumin").val().substr(4, 2);
         var birth = new Date(birthYear, birthMonth, birthDate);
         $("#birth-year").val(birthYear);
         $("#birth-month").val(birthMonth);
